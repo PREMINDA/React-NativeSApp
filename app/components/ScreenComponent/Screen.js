12bbae3b1f180c29da1b2afa5e897b5315b7cd25
PREMINDA <premindabandara0777@gmail.com>
2021-06-27 13:57:01 +0530

    Reusable Screen

---
+++ app/components/ScreenComponent/Screen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import Constants from "expo-constants";
+
+function Screen(props) {
+  return <SafeAreaView style={styles.area}>{props.children}</SafeAreaView>;
+}
+
+const styles = StyleSheet.create({
+  area: {
+    paddingTop: Constants.statusBarHeight,
+  },
+});
+
+export default Screen;
--- app/screen/MessageScree.js
+++ app/screen/MessageScree.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { FlatList } from "react-native";
+import { FlatList, SafeAreaView, StyleSheet } from "react-native";
 import Listitem from "../components/Listitem/Listitem";
+import Screen from "../components/ScreenComponent/Screen";
 const messages = [
   {
     id: 1,
@@ -26,12 +27,15 @@ function MessageScree(props) {
   );
 
   return (
-    <FlatList
-      data={messages}
-      keyExtractor={(messages) => messages.id.toString()}
-      renderItem={renderItem}
-    />
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(messages) => messages.id.toString()}
+        renderItem={renderItem}
+      />
+    </Screen>
   );
 }
+const styles = StyleSheet.create({});
 
 export default MessageScree;
